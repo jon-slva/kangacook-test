fc16558d8edf4323745da3c7d403134086b74743
Jonathan Silva <jonathan.silva167@gmail.com>
2024-08-23 21:17:30 -0400

    Add axios dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import './App.scss'
 
 export default function App() {
 	const [count, setCount] = useState(0)
@@ -17,17 +19,7 @@ export default function App() {
 				</a>
 			</div>
 			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
+
 		</>
 	)
 }
